0a39b8702e8c4959330c67437a34e74429a499a2
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-16 13:36:36 -0500

    vars demo

--- variable/var.tf
+++ variable/var.tf
@@ -7,7 +7,14 @@ variable "x" {
 # Declare the variable without value
 #variable "y" {}
 
+variable "y_list" {
+  default = {1,2,3}
+}
+
 # print variable
 output "x" {
   value = var.x
+}
+output "y" {
+  value = var.y_list
 }
