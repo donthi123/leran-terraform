60cc9e791ec07f4deb24cb8a51a6023f0aa5923a
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-16 16:59:18 -0500

    expense v2

---
+++ expense-v2/main.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "frontend" {
+  for_each      = var.instance_types
+  ami           = var.ami
+  instance_type = each.value["instance_type"]
+  vpc_security_group_ids = var.security_groups
+  tags = {
+    Name = each.key
+  }
+}
+
+variable "ami" {
+  default = "mi-041e2ea9402c46c32"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "security_groups" {
+  default = ["sg-06fdd0c03da130266"]
+}
+
+variable "instance_types" {
+  default = {
+    frontend = {
+      instance_type = "t2.micro"
+    }
+    mysql = {
+      instance_type = "t2.small"
+    }
+    backend = {
+      instance_type = "t2.small"
+    }
+  }
+}
