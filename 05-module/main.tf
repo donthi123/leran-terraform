e33b747303ce4c9a47a9e700184397e9c085d01f
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-16 16:21:28 -0500

    demo of data and module

---
+++ 05-module/main.tf
@@ -0,0 +1,8 @@
+module "demo-1"
+  source = "./demo"
+  Name = "demo-1"
+}
+module "demo-2"
+  source = "./demo"
+  Name = "demo-2"
+}
--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,26 +1,36 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-06fdd0c03da130266"]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
   tags = {
     Name = "frontend"
   }
 }
 
 resource "aws_instance" "backend" {
-  ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-06fdd0c03da130266"]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
   tags = {
     Name = "backend"
   }
 }
 
 resource "aws_instance" "mysql" {
-  ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-06fdd0c03da130266"]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
   tags = {
     Name = "mysql"
   }
+}
+
+variable "ami" {
+  default = "mi-041e2ea9402c46c32"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "security_groups" {
+  default = ["sg-06fdd0c03da130266"]
 }
