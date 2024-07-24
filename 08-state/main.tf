614ddaa039b375be609ff3ca0842cd87e1b205d3
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-23 19:57:40 -0500

    state file demo

--- 08-state/main.tf
+++ 08-state/main.tf
@@ -5,5 +5,5 @@ terraform {
     region = "us-east-1"
   }
 }
-resource "null_resource" "dummy" {}
-resource "null_resource" "dummy" {}
+resource "null_resource" "dummy1" {}
+resource "null_resource" "dummy2" {}
