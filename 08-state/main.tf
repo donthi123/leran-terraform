bbde9c515eb73a40e81f2dce9ee5b600fdebd087
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-23 19:54:55 -0500

    state file demo

---
+++ 08-state/main.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "donthi-tf-states"
+    key = "testing/state"
+    region = "us-east-1"
+  }
+}
+resource "null_resource" "dummy" {}
+resource "null_resource" "dummy" {}
